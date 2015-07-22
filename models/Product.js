94add574c16a6542115d399d0f947e14d403b016
endows <you@example.com>
2015-07-22 18:36:03 +0900

    formを整える

--- models/Product.js
+++ models/Product.js
@@ -2,16 +2,22 @@ Products = new Mongo.Collection("products");
 Products.attachSchema(new SimpleSchema({
   title: {
     type: String,
-    label: "Title",
+    label: "タイトル",
+    max: 50
+  },
+  desc: {
+    type: String,
+    label: "説明",
     max: 200
   },
   icon: {
     type: String,
+    label:"アイコン",
     autoform: {
       afFieldInput: {
         type: 'fileUpload',
         collection: 'Images',
-        label: 'Choose file'
+        label: '画像をアップロード'
       }
     }
   }
